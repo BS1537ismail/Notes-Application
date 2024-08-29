3dbcfeaa3975da620154daeba7c8d69ba03610f4
Ismail <md.ismail@brainstation-23.com>
2024-08-29 12:32:59 +0600

    add Models and Complete migration

---
+++ Notes Application/Models/Data/NotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Notes_Application.Models.Entities;
+
+namespace Notes_Application.Models.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Note> Notes { get; set; }
+    }
+}

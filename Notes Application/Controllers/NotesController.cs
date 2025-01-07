ac9afad3aa4dd01573951879f2314cd97ad873e7
Md Ismail Hosen <md.ismail@brainstation-23.com>
2025-01-07 19:22:25 +0600

    add v1 in controller

--- Notes Application/Controllers/NotesController.cs
+++ Notes Application/Controllers/NotesController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Notes_Application.Models.Entities;
 
-[Route("api/[controller]")]
+[Route("api/v1/[controller]")]
 [ApiController]
 public class NotesController : ControllerBase
 {
